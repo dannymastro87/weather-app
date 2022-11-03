ecc0f146a15a3df7a1023a5f27ca0756ae0ad52c
dannymastro87 <daniele@design01.it>
2022-11-03 09:13:31 +0100

    server-package

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <h1>Weather App</h1>
+    <div class="enter-city">
+      <input 
+      v-model="data.city"
+      placeholder="Enter a city"
+      type="text">
+    </div>
+
+    <div class="weather">
+      <h1>6&deg;</h1>
+      <h2>Cloudy</h2>
+      <h3>Lots of clouds</h3>
+    </div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { reactive } from 'vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  setup() {
+    let data = reactive({
+      city: '',
+      weather: null
+    })
+
+    return {
+      data
+    }
   }
 }
 </script>
+
+<style lang="sass">
+  .enter-city
+    input
+      font-size: 40px
+  .weather
+    margin-top: 10px
+    h1
+      font-size: 80px
+    h1, h2, h3 
+      margin: 0
+</style>
